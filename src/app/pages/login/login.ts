4e1405e1970ea022bb4eac4780c33b368f59b758
Your Name <ekka.beaula@gmail.com>
2025-09-10 23:12:07 +0200

    post password

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
@@ -17,5 +18,25 @@ export class Login {
     // You can add authentication logic here
   }
 
-  loginForm: FormGroup = new FormGroup({});
+  loginForm: FormGroup = new FormGroup({
+    EmailId: new FormControl(''),
+    Password: new FormControl(''),
+  });
+
+  http = inject(HttpClient);
+  onLogin() {
+    const formValue = this.loginForm.value;
+    this.http.post('https://localhost:44369/api/User/Login', formValue).subscribe({
+      next: (response: any) => {
+        if (response.result) {
+          alert('Login Successful');
+        } else {
+          alert('Invalid Credentials');
+        }
+      },
+      error: (err) => {
+        alert(err.error);
+      },
+    });
+  }
 }
