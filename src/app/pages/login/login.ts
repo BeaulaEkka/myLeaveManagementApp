da94c257d5d102d4c745f58dfd891d17f0df776a
Your Name <ekka.beaula@gmail.com>
2025-09-10 15:09:08 +0200

    loginsetup

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -4,8 +4,15 @@ import { Component } from '@angular/core';
   selector: 'app-login',
   imports: [],
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  styleUrl: './login.css',
 })
 export class Login {
+  email: string = '';
+  password: string = '';
 
+  onSubmit() {
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+    // You can add authentication logic here
+  }
 }
