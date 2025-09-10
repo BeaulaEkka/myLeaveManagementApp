7ac573b2d80544609a9636284166dd84f9494cb9
Your Name <ekka.beaula@gmail.com>
2025-09-10 15:28:58 +0200

    loginstyling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './pages/login/login';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,7 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
   protected readonly title = signal('myLeaveManagementApp');
 }
