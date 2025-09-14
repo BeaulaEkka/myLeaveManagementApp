ec773a9e2b091e3880ec83f09a3413615d30600a
Your Name <ekka.beaula@gmail.com>
2025-09-14 23:45:11 +0200

    auth

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Login } from './pages/login/login';
+
 import { Navbar } from './navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Login],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('myLeaveManagementApp');
+
+  isLoginPage(): boolean {
+    return this.router.url === '/login';
+  }
 }
