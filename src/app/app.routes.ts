44979758817a8d777e8fa8c44a40f391728e6b82
Your Name <ekka.beaula@gmail.com>
2025-09-18 21:46:50 +0200

    dashboard works

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,11 @@
+// app.routes.ts
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Dashboard } from './pages/dashboard/dashboard';
 
 export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full',
-  },
-  {
-    path: 'login',
-    component: Login,
-  },
-  {
-    path: '',
-    component: Login,
-    children: [
-      {
-        path: 'dashboard',
-        component: Dashboard,
-      },
-    ],
-  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'dashboard', component: Dashboard },
+  { path: '**', redirectTo: 'login' },
 ];
